bb05ec4d91f639f20dac7d8e9e9fc760f2f23c7f
theviderlab <github.account@viderlab.com>
2023-09-26 11:35:08 +0200

    fix

--- functions.php
+++ functions.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * <pluginName>
+ * <themeName>
  *
  * This file adds functions to the theme.
  *
- * @package <pluginName>
- * @author  <pluginAuthor>
+ * @package <themeName>
+ * @author  <themeAuthor>
  * @license GPL-2.0-or-later
- * @link    <pluginAuthorURI>
+ * @link    <themeAuthorURI>
  */
 
 // Starts the engine.
